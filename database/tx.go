8895ece26a3380bcf539538474183bdef0de1c7d
Marc Watters <marc.watters@proton.me>
2025-02-21 01:14:24 -0500

    03 - feat(db): add type Tx with constructor

--- database/tx.go
+++ database/tx.go
@@ -5,3 +5,14 @@ type Account string
 func NewAccount(value string) Account {
 	return Account(value)
 }
+
+type Tx struct {
+	From  Account `json:"from"`
+	To    Account `json:"to"`
+	Value uint    `json:"value"`
+	Data  string  `json:"data"`
+}
+
+func NewTx(from Account, to Account, value uint, data string) Tx {
+	return Tx{from, to, value, data}
+}

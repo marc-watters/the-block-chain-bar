d0df46cbf6e7a94b73e181a7b907fa6c4fdef36e
Marc Watters <marc.watters@proton.me>
2025-02-21 04:15:50 -0500

    03 - feat(db): add func NewStateFromDisk()

--- database/state.go
+++ database/state.go
@@ -1,10 +1,92 @@
 package database
 
-import "github.com/spf13/afero"
+import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/afero"
+)
+
+var AppFs *afero.Afero
 
 type State struct {
 	Balances map[Account]uint
 
 	txMempool []Tx
-	db        *afero.File
+	db        afero.File
+}
+
+const (
+	Dir  = "database"
+	GenF = "genesis.json"
+	TxF  = "tx.db"
+)
+
+func NewStateFromDisk() (*State, error) {
+	// load genesis file
+	gf, err := AppFs.ReadFile(filepath.Join(Dir, GenF))
+	if err != nil {
+		return nil, err
+	}
+
+	// load transaction file
+	txf, err := AppFs.OpenFile(filepath.Join(Dir, TxF), os.O_APPEND|os.O_RDWR, 0600)
+	if err != nil {
+		return nil, err
+	}
+
+	// unmarsall genesis data into struct
+	var g struct {
+		Balances map[Account]uint `json:"balances"`
+	}
+	if err := json.Unmarshal(gf, &g); err != nil {
+		return nil, fmt.Errorf("unmarshall genesis: %v", err)
+	}
+
+	// create state object
+	s := &State{
+		Balances:  make(map[Account]uint),
+		txMempool: make([]Tx, 0),
+		db:        txf,
+	}
+
+	// populate state balances
+	for account, balance := range g.Balances {
+		s.Balances[account] = balance
+	}
+
+	// process transactions
+	if _, err := s.db.Seek(0, io.SeekStart); err != nil {
+		return nil, err
+	}
+
+	scanner := bufio.NewScanner(s.db)
+	for scanner.Scan() {
+		if err := scanner.Err(); err != nil {
+			return nil, fmt.Errorf("tx scan failed: %v", err)
+		}
+
+		var tx Tx
+		if err := json.Unmarshal(scanner.Bytes(), &tx); err != nil {
+			return nil, fmt.Errorf("unmarshall transaction: %v", err)
+		}
+
+		if tx.IsReward() {
+			s.Balances[tx.To] += tx.Value
+			continue
+		}
+
+		if s.Balances[tx.From] < tx.Value {
+			return nil, fmt.Errorf("insufficient balance")
+		}
+
+		s.Balances[tx.From] -= tx.Value
+		s.Balances[tx.To] += tx.Value
+	}
+
+	return s, nil
 }
---
+++ database/state_test.go
@@ -0,0 +1,58 @@
+package database_test
+
+import (
+	"os"
+	"path/filepath"
+	"tbb/v2/database"
+	"testing"
+
+	"github.com/spf13/afero"
+)
+
+var appFs *afero.Afero
+
+func init() {
+	database.AppFs = &afero.Afero{Fs: afero.NewMemMapFs()}
+	appFs = database.AppFs
+}
+
+var (
+	dir  = database.Dir
+	genF = filepath.Join(dir, database.GenF)
+	txF  = filepath.Join(dir, database.TxF)
+)
+
+func TestNewStateFromDisk(t *testing.T) {
+	gd := []byte(`{"balances": {"A": 1, "B": 0}}`)
+	if err := appFs.WriteFile(genF, gd, os.ModeAppend); err != nil {
+		t.Fatalf("error writing to %q: %v", genF, err)
+	}
+
+	txd := []byte(`{"from": "A", "to": "B", "value": 1}`)
+	if err := appFs.WriteFile(txF, txd, os.ModeAppend); err != nil {
+		t.Fatalf("error writing to %q: %v", txF, err)
+	}
+
+	s, err := database.NewStateFromDisk()
+	if err != nil {
+		t.Fatalf("error loading state: %v", err)
+	}
+
+	a, ok := s.Balances["A"]
+	if !ok {
+		t.Errorf("assert state account failed: could not find account 'A'")
+	}
+
+	b, ok := s.Balances["B"]
+	if !ok {
+		t.Errorf("assert state account failed: could not find account 'B'")
+	}
+
+	if a != 0 {
+		t.Errorf("assert state balance failed: wrong balance for account 'A': %d", a)
+	}
+
+	if b != 1 {
+		t.Errorf("assert state balance failed: wrong balance for account 'B': %d", a)
+	}
+}

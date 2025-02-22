cdb33552a707e9ebaf80209789daa083f18cc5ad
Marc Watters <marc.watters@proton.me>
2025-02-22 00:58:36 -0500

    03 - feat(cli): add command balances list

--- cmd/tbb/balances.go
+++ cmd/tbb/balances.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"tbb/v2/database"
+	"text/tabwriter"
 
 	"github.com/spf13/cobra"
 )
@@ -16,5 +19,33 @@ func balancesCmd() *cobra.Command {
 		Run: func(cmd *cobra.Command, args []string) {},
 	}
 
+	balancesCmd.AddCommand(balancesListCmd)
+
 	return balancesCmd
+}
+
+var balancesListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all balances",
+	Run: func(cmd *cobra.Command, args []string) {
+		s, err := database.NewStateFromDisk()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer s.Close()
+
+		fmt.Println()
+		fmt.Println("    Account Balances    ")
+		fmt.Println("________________________")
+
+		w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+		for a, b := range s.Balances {
+			fmt.Fprintf(w, "* %s\t|\t%d\n", a, b)
+		}
+		w.Flush()
+
+		fmt.Println("------------------------")
+		fmt.Println()
+	},
 }

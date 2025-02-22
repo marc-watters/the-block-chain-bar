3217500f9f07e8025abd84daf86996ecaea0c589
Marc Watters <marc.watters@proton.me>
2025-02-22 16:12:43 -0500

    03 - feat(test): assert error insufficient balance

--- database/state_test.go
+++ database/state_test.go
@@ -23,36 +23,59 @@ var (
 )
 
 func TestNewStateFromDisk(t *testing.T) {
-	gd := []byte(`{"balances": {"A": 1, "B": 0}}`)
-	if err := appFs.WriteFile(genF, gd, os.ModeAppend); err != nil {
-		t.Fatalf("error writing to %q: %v", genF, err)
-	}
+	t.Run("assert accounts and balances from new state", func(t *testing.T) {
+		gd := []byte(`{"balances": {"A": 1, "B": 0}}`)
+		if err := appFs.WriteFile(genF, gd, os.ModeAppend); err != nil {
+			t.Fatalf("error writing to %q: %v", genF, err)
+		}
 
-	txd := []byte(`{"from": "A", "to": "B", "value": 1}`)
-	if err := appFs.WriteFile(txF, txd, os.ModeAppend); err != nil {
-		t.Fatalf("error writing to %q: %v", txF, err)
-	}
+		txd := []byte(`{"from": "A", "to": "B", "value": 1}`)
+		if err := appFs.WriteFile(txF, txd, os.ModeAppend); err != nil {
+			t.Fatalf("error writing to %q: %v", txF, err)
+		}
 
-	s, err := database.NewStateFromDisk()
-	if err != nil {
-		t.Fatalf("error loading state: %v", err)
-	}
+		s, err := database.NewStateFromDisk()
+		if err != nil {
+			t.Fatalf("error loading state: %v", err)
+		}
 
-	a, ok := s.Balances["A"]
-	if !ok {
-		t.Errorf("assert state account failed: could not find account 'A'")
-	}
+		a, ok := s.Balances["A"]
+		if !ok {
+			t.Errorf("assert state account failed: could not find account 'A'")
+		}
 
-	b, ok := s.Balances["B"]
-	if !ok {
-		t.Errorf("assert state account failed: could not find account 'B'")
-	}
+		b, ok := s.Balances["B"]
+		if !ok {
+			t.Errorf("assert state account failed: could not find account 'B'")
+		}
 
-	if a != 0 {
-		t.Errorf("assert state balance failed: wrong balance for account 'A': %d", a)
-	}
+		if a != 0 {
+			t.Errorf("assert state balance failed: wrong balance for account 'A': %d", a)
+		}
 
-	if b != 1 {
-		t.Errorf("assert state balance failed: wrong balance for account 'B': %d", a)
-	}
+		if b != 1 {
+			t.Errorf("assert state balance failed: wrong balance for account 'B': %d", a)
+		}
+	})
+
+	t.Run("assert error insufficent balance", func(t *testing.T) {
+		gd := []byte(`{"balances": {"A": 1, "B": 0}}`)
+		if err := appFs.WriteFile(genF, gd, os.ModeAppend); err != nil {
+			t.Fatalf("error writing to %q: %v", genF, err)
+		}
+
+		txd := []byte(`{"from": "A", "to": "B", "value": 2}`)
+		if err := appFs.WriteFile(txF, txd, os.ModeAppend); err != nil {
+			t.Fatalf("error writing to %q: %v", txF, err)
+		}
+
+		_, err := database.NewStateFromDisk()
+		if err == nil {
+			t.Errorf("assert insufficient balance failed, expected an error")
+		}
+
+		if err.Error() != "insufficient balance" {
+			t.Errorf("assert insufficient balance failed, unexpected error: %v", err)
+		}
+	})
 }

fef9fba0a339c09a082f15a6e80d6ad366ec6e8a
Marc Watters <marc.watters@proton.me>
2025-02-22 09:01:48 -0500

    03 - feat(cli): add command tx

---
+++ cmd/tbb/tx.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+func txCmd() *cobra.Command {
+	txsCmd := &cobra.Command{
+		Use:   "tx",
+		Short: "Interact with transactions",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return fmt.Errorf("incorrect usage")
+		},
+		Run: func(cmd *cobra.Command, args []string) {},
+	}
+
+	return txsCmd
+}

2c5f1acb682a6bdde9d941479fbc6c5793f65b81
Marc Watters <marc.watters@proton.me>
2025-02-23 15:09:49 -0500

    07 - feat(test): assert error insufficient balance

--- database/state_test.go
+++ database/state_test.go
@@ -1,10 +1,12 @@
 package database_test
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"tbb/v2/database"
 	"testing"
+	"time"
 
 	"github.com/spf13/afero"
 )
@@ -43,6 +45,43 @@ func TestNewStateFromDisk(t *testing.T) {
 
 		assertAccount(t, s, a, 0)
 		assertAccount(t, s, b, 1)
+	})
+
+	t.Run("assert error insufficent balance", func(t *testing.T) {
+		genData := []byte(`{"balances": {"A": 0, "B": 1}}`)
+		if err := appFs.WriteFile(genF, genData, os.ModeAppend); err != nil {
+			t.Fatalf("error writing to genesis file: %v", err)
+		}
+
+		if err := appFs.WriteFile(txF, []byte{}, os.ModeAppend); err != nil {
+			t.Fatalf("error writing to transaction file: %v", err)
+		}
+
+		s, err := database.NewStateFromDisk()
+		if err != nil {
+			t.Fatalf("error loading state: %v", err)
+		}
+
+		a := database.NewAccount("A")
+		b := database.NewAccount("B")
+
+		block := database.NewBlock(
+			database.Hash{},
+			uint64(time.Now().Unix()),
+			[]database.Tx{
+				database.NewTx(a, b, 1, ""),
+			},
+		)
+
+		err = s.AddBlock(block)
+		if err == nil {
+			t.Fatal("assert insufficent balance failed, expected an error")
+		}
+
+		var insufficientBalance database.ErrInsufficientBalance
+		if !errors.As(err, &insufficientBalance) {
+			t.Errorf("assert insufficient balance failed, unexpected error: %T", err)
+		}
 	})
 }
 

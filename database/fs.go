a7ff07f6088acf5e20d5666f76aa44ee6b28ff9e
Marc Watters <marc.watters@proton.me>
2025-02-23 20:07:23 -0500

    08 - feat(db): add helper getBlocksDbFilePath()

--- database/fs.go
+++ database/fs.go
@@ -9,3 +9,7 @@ func getDatabaseDirPath(dataDir string) string {
 func getGenesisJsonFilePath(dataDir string) string {
 	return filepath.Join(getDatabaseDirPath(dataDir), GenF)
 }
+
+func getBlocksDbFilePath(dataDir string) string {
+	return filepath.Join(getDatabaseDirPath(dataDir), TxF)
+}

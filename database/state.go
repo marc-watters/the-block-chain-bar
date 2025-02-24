0479f1f745f4900a90e48d95a156d91a69a53205
Marc Watters <marc.watters@proton.me>
2025-02-24 08:59:23 -0500

    07 - feat(db): implement State.Persist()

--- database/state.go
+++ database/state.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"path/filepath"
+	"time"
 
 	"github.com/spf13/afero"
 )
@@ -25,7 +26,7 @@ type State struct {
 const (
 	Dir  = "database"
 	GenF = "genesis.json"
-	TxF  = "tx.db"
+	TxF  = "block.db"
 )
 
 func NewStateFromDisk() (*State, error) {
@@ -91,8 +92,36 @@ func (s *State) Add(tx Tx) error {
 	return nil
 }
 
+func (s *State) Persist() (Hash, error) {
+	b := NewBlock(s.latestBlockHash, uint64(time.Now().Unix()), s.txMempool)
+	bh, err := b.Hash()
+	if err != nil {
+		return Hash{}, err
+	}
+
+	bfs := BlockFS{bh, b}
+
+	bfsJson, err := json.Marshal(bfs)
+	if err != nil {
+		return Hash{}, err
+	}
+
+	fmt.Printf("Persisting new Block to disk:\n")
+	fmt.Printf("\t%s\n", bfsJson)
+
+	if _, err := s.db.Write(append(bfsJson, '\n')); err != nil {
+		return Hash{}, err
+	}
+
+	s.latestBlockHash = bh
+
+	s.txMempool = []Tx{}
+
+	return s.latestBlockHash, nil
+}
+
 func (s *State) LatestHash() Hash {
-	return s.latestHash
+	return s.latestBlockHash
 }
 
 func (s *State) Close() {

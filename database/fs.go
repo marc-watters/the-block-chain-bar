a9a193ef164bb81ad1fb646cb584d6c684a7dbca
Marc Watters <marc.watters@proton.me>
2025-02-23 20:05:17 -0500

    08 - feat(db): add helper getGenesisJsonFilePath()

--- database/fs.go
+++ database/fs.go
@@ -5,3 +5,7 @@ import "path/filepath"
 func getDatabaseDirPath(dataDir string) string {
 	return filepath.Join(dataDir, Dir)
 }
+
+func getGenesisJsonFilePath(dataDir string) string {
+	return filepath.Join(getDatabaseDirPath(dataDir), GenF)
+}

334313b8f791fe2e20d6577d558c02d2d1caa1dd
Marc Watters <marc.watters@proton.me>
2025-03-03 17:33:01 -0500

    03 - feat(cli)[+]: Command tbb

---
+++ cmd/tbb/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	tbbCmd := &cobra.Command{
+		Use:   "tbb",
+		Short: "The Blockchain Bar CLI",
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return fmt.Errorf("incorrect usage")
+		},
+		Run: func(cmd *cobra.Command, args []string) {},
+	}
+
+	err := tbbCmd.Execute()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,13 @@ module github.com/marc-watters/the-block-chain-bar/v2
 
 go 1.24.0
 
-require github.com/spf13/afero v1.12.0
+require (
+	github.com/spf13/afero v1.12.0
+	github.com/spf13/cobra v1.9.1
+)
 
-require golang.org/x/text v0.21.0 // indirect
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+	golang.org/x/text v0.21.0 // indirect
+)

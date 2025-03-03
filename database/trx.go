6ea74af6b30dda38e42c6c4820298a6fc5fe018a
Marc Watters <marc.watters@proton.me>
2025-03-03 18:57:14 -0500

    03 - rfxr(db)[+]: Type Trx with constructor

--- database/trx.go
+++ database/trx.go
@@ -2,8 +2,21 @@ package database
 
 import "strings"
 
-type Account string
+type (
+	Account string
+
+	Trx struct {
+		From  Account
+		To    Account
+		Value uint64
+		Data  string
+	}
+)
 
 func NewAccount(value string) Account {
 	return Account(strings.ToLower(value))
+}
+
+func NewTrx(from Account, to Account, value uint64, data string) Trx {
+	return Trx{from, to, value, data}
 }

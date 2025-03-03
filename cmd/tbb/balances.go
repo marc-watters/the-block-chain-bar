e8f87c54cf04cfca01b4ba40000ccb8faf80037c
Marc Watters <marc.watters@proton.me>
2025-03-03 18:23:24 -0500

    03 - feat(cli)[+]: Command tbb balances list

--- cmd/tbb/balances.go
+++ cmd/tbb/balances.go
@@ -1,11 +1,54 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
+
+	"github.com/spf13/cobra"
+
+	"github.com/marc-watters/the-block-chain-bar/v2/database"
+)
 
 func balancesCmd() *cobra.Command {
-	return &cobra.Command{
+	balancesCmd := &cobra.Command{
 		Use:   "balances",
 		Short: "Interact with balances",
-		Run:   func(cmd *cobra.Command, args []string) {},
+		PreRunE: func(cmd *cobra.Command, args []string) error {
+			return fmt.Errorf("incorrect usage")
+		},
+		Run: func(cmd *cobra.Command, args []string) {},
 	}
+
+	balancesCmd.AddCommand(balancesListCmd)
+
+	return balancesCmd
+}
+
+var balancesListCmd = &cobra.Command{
+	Use:   "list",
+	Short: "Lists all balances",
+	Run: func(cmd *cobra.Command, args []string) {
+		s, err := database.NewStateFromDisk()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer s.Close()
+
+		fmt.Println()
+		fmt.Printf("%s\n", strings.Repeat("=", 72))
+		fmt.Printf("%[1]s Account Balances %[1]s\n", strings.Repeat(" ", 27))
+		fmt.Printf("%s\n", strings.Repeat("=", 72))
+
+		w := tabwriter.NewWriter(os.Stdout, 1, 1, 1, ' ', 0)
+		for k, v := range s.Balances {
+			fmt.Fprintf(w, " |>\t%s\t%d\n", k, v)
+		}
+		w.Flush()
+
+		fmt.Printf("%s", strings.Repeat("-", 72))
+		fmt.Println()
+	},
 }

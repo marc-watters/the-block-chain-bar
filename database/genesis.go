6ca4dbb238853eb3e68d754d23a80f171b2cabf2
Marc Watters <marc.watters@proton.me>
2025-03-04 02:07:35 -0500

    03 - rfxr(db)[+]: Helper loadGenesis()

--- database/genesis.go
+++ database/genesis.go
@@ -1,5 +1,22 @@
 package database
 
+import "encoding/json"
+
 type genesis struct {
 	Balances map[string]uint64 `json:"balances"`
+}
+
+func loadGenesis(path string) (genesis, error) {
+	content, err := AppFS.ReadFile(path)
+	if err != nil {
+		return genesis{}, err
+	}
+
+	var loadedGenesis genesis
+	err = json.Unmarshal(content, &loadedGenesis)
+	if err != nil {
+		return genesis{}, err
+	}
+
+	return loadedGenesis, nil
 }

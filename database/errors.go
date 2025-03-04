0a4b465de58a422debc4de4f82ab601840ff3a55
Marc Watters <marc.watters@proton.me>
2025-03-04 13:38:56 -0500

    03 - rfxr(db)[+]: Type ErrInvalidTransaction

--- database/errors.go
+++ database/errors.go
@@ -1,11 +1,26 @@
 package database
 
-type ErrInsufficientBalance struct {
-	From  Account
-	To    Account
-	Value uint64
+import "fmt"
+
+type (
+	ErrInsufficientBalance struct {
+		From  Account
+		To    Account
+		Value uint64
+	}
+	ErrInvalidTransaction struct {
+		field string
+	}
+)
+
+func NewInvalidTransaction(field string) ErrInvalidTransaction {
+	return ErrInvalidTransaction{field}
 }
 
 func (e ErrInsufficientBalance) Error() string {
 	return "insufficient balance"
+}
+
+func (e ErrInvalidTransaction) Error() string {
+	return fmt.Sprintf("invalid value for field: '%s'", e.field)
 }

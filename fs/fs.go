04fffcee52a59be4ad76b22ccebb267cf61f65f8
Marc Watters <marc.watters@proton.me>
2025-03-04 23:41:44 -0500

    08 - feat(fs)[+]: Genesis JSON string

--- fs/fs.go
+++ fs/fs.go
@@ -13,3 +13,12 @@ var AppFS *afero.Afero
 func init() {
 	AppFS = &afero.Afero{Fs: afero.NewOsFs()}
 }
+
+var genesisJSON = `
+{
+  "genesis_time": "2019-03-18T00:00:00.000000000Z",
+  "chain_id": "the-blockchain-bar-ledger",
+  "balances": {
+    "andrej": 1000000
+  }
+}`

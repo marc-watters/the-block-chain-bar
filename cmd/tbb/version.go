2aa10afbdf407ea9d08d41528fcc233f9eb71bf3
Marc Watters <marc.watters@proton.me>
2025-03-05 13:23:23 -0500

    03 - build(cli)[+]: v0.2.0 - Add command balances

--- cmd/tbb/version.go
+++ cmd/tbb/version.go
@@ -8,9 +8,9 @@ import (
 
 const (
 	Major  = "0"
-	Minor  = "1"
+	Minor  = "2"
 	Fix    = "0"
-	Verbal = "Add command tbb version"
+	Verbal = "Add command balances"
 )
 
 func versionCmd() *cobra.Command {

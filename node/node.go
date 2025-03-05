8f19605bc462acc6eed45bb1990c59b0d41f7067
Marc Watters <marc.watters@proton.me>
2025-03-05 18:39:28 -0500

    08 - feat(node)[+]: Node.Run()

--- node/node.go
+++ node/node.go
@@ -1,5 +1,10 @@
 package node
 
+import (
+	"fmt"
+	"net/http"
+)
+
 type (
 	Node struct {
 		state state
@@ -9,4 +14,13 @@ type (
 
 func New(s state) *Node {
 	return &Node{s}
+}
+
+func (n *Node) Run() error {
+	const port = 8080
+
+	mx := http.NewServeMux()
+
+	fmt.Printf("Listening on %s:%d", "127.0.0.1\n", port)
+	return http.ListenAndServe(fmt.Sprintf(":%d", port), mx)
 }

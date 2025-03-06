0e94907fad7ffef55d35d8e201da177b2ad1d189
Marc Watters <marc.watters@proton.me>
2025-03-06 10:00:52 -0500

    08 - feat(node)[+]: Node.GetBalances()

--- node/node.go
+++ node/node.go
@@ -1,8 +1,10 @@
 package node
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 
 	db "github.com/marc-watters/the-block-chain-bar/v2/database"
 )
@@ -28,4 +30,25 @@ func (n *Node) Run() error {
 
 	fmt.Printf("Listening on %s:%d", "127.0.0.1\n", port)
 	return http.ListenAndServe(fmt.Sprintf(":%d", port), mx)
+}
+
+func (n *Node) GetBalances(w http.ResponseWriter, r *http.Request) {
+	res := struct {
+		Hash     db.Hash               `json:"block_hash"`
+		Balances map[db.Account]uint64 `json:"balances"`
+	}{
+		n.state.LatestBlockHash(),
+		n.state.Balances(),
+	}
+
+	resJSON, err := json.Marshal(res)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if _, err := w.Write(resJSON); err != nil {
+		fmt.Fprintf(os.Stderr, "Node.GetBalances() write error: %v", err)
+		return
+	}
 }

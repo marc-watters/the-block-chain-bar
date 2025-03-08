dfaa7007274c3a4e2cb57decbcc6106b43e31712
Marc Watters <marc.watters@proton.me>
2025-03-08 08:09:25 -0500

    10 - feat(node)[+]: Helper fetchNewBlocksAndPeers()

--- node/sync.go
+++ node/sync.go
@@ -5,6 +5,32 @@ import (
 	"net/http"
 )
 
+func (n *Node) fetchNewBlocksAndPeers() {
+	for _, knownPeer := range n.knownPeers {
+		status, err := queryPeerStatus(knownPeer)
+		if err != nil {
+			fmt.Printf("ERROR: %s\n", err)
+			continue
+		}
+
+		localBlockHeight := n.state.LatestBlock().Header.Height
+		if localBlockHeight < status.Height {
+			newBlocksCount := status.Height - localBlockHeight
+
+			fmt.Printf("Found %d new blocks from Peer %s\n", newBlocksCount, knownPeer.IP)
+		}
+
+		for _, peer := range status.KnownPeers {
+			newPeer, isKnownPeer := n.knownPeers[peer.Address()]
+			if !isKnownPeer {
+				fmt.Printf("Found new Peer %s\n", newPeer.Address())
+
+				n.knownPeers[newPeer.Address()] = newPeer
+			}
+		}
+	}
+}
+
 func queryPeerStatus(peer PeerNode) (StatusRes, error) {
 	url := fmt.Sprintf("http://%s/%s", peer.Address(), endpointStatus)
 	res, err := http.Get(url)

c66e5ffb12d3f2fa94f5b70d91e2eeb110298380
Marc Watters <marc.watters@proton.me>
2025-03-08 12:40:37 -0500

    10 - feat(db)[+]: Helper GetBlocksAfter()

---
+++ database/database.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"bufio"
+	"encoding/json"
+	"os"
+
+	"github.com/marc-watters/the-block-chain-bar/v2/fs"
+)
+
+func GetBlocksAfter(blockHash Hash, dataDir string) ([]Block, error) {
+	f, err := os.OpenFile(fs.GetBlocksDBFilePath(dataDir), os.O_RDONLY, 0o600)
+	if err != nil {
+		return nil, err
+	}
+
+	blocks := make([]Block, 0)
+	shouldStartCollecting := false
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if err = scanner.Err(); err != nil {
+			return nil, err
+		}
+
+		var blockFs BlockFS
+		err = json.Unmarshal(scanner.Bytes(), &blockFs)
+		if err != nil {
+			return nil, err
+		}
+
+		if shouldStartCollecting {
+			blocks = append(blocks, blockFs.Value)
+			continue
+		}
+
+		if blockHash == blockFs.Key {
+			shouldStartCollecting = true
+		}
+	}
+
+	return blocks, nil
+}

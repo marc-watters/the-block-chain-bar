84a5a45848535c022b9f8128ba000438515705d9
Marc Watters <marc.watters@proton.me>
2025-03-08 05:39:23 -0500

    10 - rfxr(fs)[+]: Helper homeDir()

--- fs/fs.go
+++ fs/fs.go
@@ -104,3 +104,13 @@ func ExpandPath(p string) string {
 	}
 	return path.Clean(os.ExpandEnv(p))
 }
+
+func homeDir() string {
+	if home := os.Getenv("HOME"); home != "" {
+		return home
+	}
+	if usr, err := user.Current(); err == nil {
+		return usr.HomeDir
+	}
+	return ""
+}

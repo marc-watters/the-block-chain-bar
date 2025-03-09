c305b50904c6efb29ace5c2fd384ef46c46a8992
Marc Watters <marc.watters@proton.me>
2025-03-09 11:54:47 -0400

    11 - feat(node)[+]: Type PendingBlock with constructor

---
+++ node/miner.go
@@ -0,0 +1,19 @@
+package node
+
+import (
+	"time"
+
+	db "github.com/marc-watters/the-block-chain-bar/v2/database"
+)
+
+type PendingBlock struct {
+	parent db.Hash
+	height uint64
+	time   uint64
+	trxs   []db.Trx
+}
+
+func NewPendingBlock(parent db.Hash, height uint64, trxs []db.Trx) PendingBlock {
+	t := uint64(time.Now().Unix())
+	return PendingBlock{parent, height, t, trxs}
+}

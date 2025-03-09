f2ef5f9634179dc678ba083b955c85755da6574e
Marc Watters <marc.watters@proton.me>
2025-03-09 16:20:40 -0400

    11 - test(node)[+]: Assert Node.Run()

---
+++ node/node_integration_test.go
@@ -0,0 +1,35 @@
+package node
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	db "github.com/marc-watters/the-block-chain-bar/v2/database"
+	"github.com/marc-watters/the-block-chain-bar/v2/fs"
+)
+
+const peerNodePort = 8081
+
+func TestNode_Run(t *testing.T) {
+	dataDir := getTestDataDirPath(t)
+	if err := fs.RemoveDir(dataDir); err != nil {
+		t.Fatalf("error cleansing data directory: %v", err)
+	}
+
+	s, err := db.NewStateFromDisk(dataDir)
+	if err != nil {
+		t.Fatalf("error creating new state from disk: %v", err)
+	}
+
+	pn := NewPeerNode(DefaultIP, peerNodePort, true, true)
+	n := New(s, DefaultIP, DefaultHTTPort, pn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	if err := n.Run(ctx); err.Error() != "http: Server closed" {
+		cancel()
+		t.Errorf("expected node to shutdown after 5 seconds")
+	}
+	cancel()
+}
